9dbb4f9099b62493194e8f8fa493a6bfc99a6407
vpsbash <vpsbash@gmail.com>
2022-12-02 17:44:59 +0800

    add ape cosmopolitan

---
+++ memtester_ape.c
@@ -0,0 +1,28 @@
+#include "libc/stdio/stdio.h"
+#include "libc/stdio/rand.h"
+#include "libc/mem/mem.h"
+#include "libc/time/time.h"
+
+int main()
+{
+    printf("I will try my best to fill your RAM.\n");
+    printf("Enter y to continue the test\n");
+    char check;
+    scanf("%c", &check);
+    if ('y' != check && 'Y' != check)
+        return 1;
+    long allocatedMB = 0;
+    srand((unsigned int)time(NULL));
+    while (1)
+    {
+        unsigned char* leakThisMemoryPlease = (unsigned char*)malloc(1048576);
+        int i;
+        for (i = 0; i < 1048576; i++)
+            leakThisMemoryPlease[i] = i * rand();
+        allocatedMB += 1;
+        printf("%ld MB allocated\n", allocatedMB);
+        if (allocatedMB < 0)
+            break;
+    }
+    return 0;
+}
